a8e629491099a5579dafcd70f882b7d92efa15b1
Kirill Shchitaev <67977498+shchitaev@users.noreply.github.com>
2020-11-06 00:51:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const http = require('http');
-const CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'x-test, Content-Type, Accept, Access-Control-Allow-Headers'
-  };
-const server = http.createServer(async(req,res)=>{
-      if (req.url === '/result4/') {
+require('http').Server((req, res) => {
+  const author = 'itmo287704'
+
+  res.setHeader('X-Author', author)
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'x-test,Content-Type,Accept,Access-Control-Allow-Headers')
+
+  if (req.url === '/result4/') {
     let body = [];
     req.on('data', (chunk) => {
       body.push(chunk);
@@ -13,12 +15,12 @@ const server = http.createServer(async(req,res)=>{
       body = Buffer.concat(body).toString();
 
       return res.end(JSON.stringify({
-        'message': 'itmo287704',
+        message: author,
         'x-result': req.headers['x-test'],
         'x-body': body,
       }))
     })
   } else {
-    res.end('itmo287704')
+    res.end(author)
   }
 }).listen(process.env.PORT)
